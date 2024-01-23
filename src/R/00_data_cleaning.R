1851f390dcaef89de14decae9c1c5c4198300cb2
colebrookson <cole.brookson@gmail.com>
2024-01-23 13:20:07 -0500

    just the one file

--- src/R/00_data_cleaning.R
+++ src/R/00_data_cleaning.R
@@ -1,14 +1,15 @@
 # Author: Cole Brookson
 # Date: 2024-01-23
 
-# Load packages 
+# Load packages
 library(magrittr)
 
 # Load data
 df <- readxl::read_excel(here::here("data", "raw", "csu_cdc_merged.xlsx"),
-                         sheet = "all",
-                         col_names = TRUE,
-                         skip = 1) %>%
+  sheet = "all",
+  col_names = TRUE, skip = 1
+) %>%
   janitor::clean_names()
-
-# Clean data
+  
+# Write data
+readr::write_csv(df, here::here("data", "clean", "csu_cdc_merged.csv"))
