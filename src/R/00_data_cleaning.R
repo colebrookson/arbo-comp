0b9ba5c0e048766d189919b8f25222347a873ed6
colebrookson <cole.brookson@gmail.com>
2024-01-23 13:14:56 -0500

    reading and cleaning

---
+++ src/R/00_data_cleaning.R
@@ -0,0 +1,14 @@
+# Author: Cole Brookson
+# Date: 2024-01-23
+
+# Load packages 
+library(magrittr)
+
+# Load data
+df <- readxl::read_excel(here::here("data", "raw", "csu_cdc_merged.xlsx"),
+                         sheet = "all",
+                         col_names = TRUE,
+                         skip = 1) %>%
+  janitor::clean_names()
+
+# Clean data
